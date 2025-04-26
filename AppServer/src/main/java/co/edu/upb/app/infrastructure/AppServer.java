2ee65315170fa5504ffb1f72e43404b4051ae1c4
David Cárdenas <santito500@live.com>
2025-04-26 17:58:22 -0500

    feat: SSL implementation for SOAP server

--- AppServer/src/main/java/co/edu/upb/app/infrastructure/AppServer.java
+++ AppServer/src/main/java/co/edu/upb/app/infrastructure/AppServer.java
@@ -4,11 +4,13 @@ import co.edu.upb.app.config.Environment;
 import co.edu.upb.app.domain.interfaces.infrastructure.InterfaceApp;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
 import com.sun.net.httpserver.HttpsServer;
 import jakarta.xml.ws.Endpoint;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
 import java.net.InetSocketAddress;
@@ -31,30 +33,40 @@ public class AppServer {
                 ks.load(fis, ksPass);
             }
 
-            //Setup the keymanager and trustmanager
-            KeyManagerFactory kmf = KeyManagerFactory
-                    .getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
             kmf.init(ks, ksPass);
-
-            TrustManagerFactory tmf = TrustManagerFactory
-                    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            tmf.init(ks);  // trust the same certs
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
 
             //SSLContext
             SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
 
-            //Create the HTTP server based on
+            //Create the HTTP server
             int port = Integer.parseInt(Environment.getInstance().getDotenv().get("SOAP_PORT"));
-            HttpsServer httpsServer = HttpsServer.create(
+            HttpsServer server = HttpsServer.create(
                     new InetSocketAddress(port), 0
             );
-            HttpContext ctx = httpsServer.createContext("/soap");
+
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                @Override
+                public void configure(HttpsParameters params) {
+                    // grab the default SSL parameters from our context
+                    SSLContext c = getSSLContext();
+                    SSLParameters sslParams = c.getDefaultSSLParameters();
+                    sslParams.setNeedClientAuth(false);
+                    params.setSSLParameters(sslParams);
+                }
+            });
+
+            //Create /appserver context and publish JAX-WS endpoint
+            HttpContext ctx = server.createContext("/appserver");
             Endpoint endpoint = Endpoint.create(service);
             endpoint.publish(ctx);
 
-            //Start server
-            httpsServer.start();
+            //Start the server
+            server.setExecutor(null);
+            server.start();
             System.out.println("App server running on port " + Environment.getInstance().getDotenv().get("SOAP_PORT"));
         } catch (Exception e) {
             e.printStackTrace();
